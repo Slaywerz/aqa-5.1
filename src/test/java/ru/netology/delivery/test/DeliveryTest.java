48deb42d9e2506a3f28a2d81428b694185172b43
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-09-23 23:18:21 +0300

    tests added

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -33,12 +33,10 @@ public class DeliveryTest {
     }
 
     @Test
-    @DisplayName("Should be reserved meet")
+    @DisplayName("Plain delivery date")
     void shouldBeReservedMeet() {
         val validUser = DataGenerator.Registration.generateUser("ru");
         val daysToAddForFirstMeeting = 4;
-        val daysToAddForSecondMeeting = 7;
-        val secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         $("[data-test-id='city'] .input__box .input__control[placeholder='Город']").setValue(validUser.getCity());
         $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(delete());
         $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(DataGenerator
@@ -50,14 +48,87 @@ public class DeliveryTest {
         $("[data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(10));
         $("[data-test-id='success-notification'][data-test-id='success-notification'] .notification__content")
                 .shouldHave(Condition.text(DataGenerator.generateDate(daysToAddForFirstMeeting)));
+    }
+
+    @Test
+    @DisplayName("Plane and Re-plain delivery date")
+    void shouldBeChangedDate(){
+            val validUser = DataGenerator.Registration.generateUser("ru");
+            val daysToAddForFirstMeeting = 4;
+            val daysToAddForSecondMeeting = 7;
+            val secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+            $("[data-test-id='city'] .input__box .input__control[placeholder='Город']").setValue(validUser.getCity());
+            $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(delete());
+            $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(DataGenerator
+                    .generateDate(daysToAddForFirstMeeting));
+            $("[data-test-id='name'] .input__box .input__control[name='name']").setValue(DataGenerator.generateFullName("ru"));
+            $("[data-test-id='phone'] .input__box .input__control[name='phone']").setValue(validUser.getPhone());
+            $("[data-test-id='agreement'] .checkbox__box").click();
+            $(byText("Запланировать")).click();
+            $("[data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(10));
+            $("[data-test-id='success-notification'][data-test-id='success-notification'] .notification__content")
+                    .shouldHave(Condition.text(DataGenerator.generateDate(daysToAddForFirstMeeting)));
+            $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(delete());
+            $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(DataGenerator
+                    .generateDate(daysToAddForSecondMeeting));
+            $(byText("Запланировать")).click();
+            $(byText("Перепланировать")).click();
+            $("[data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(10));
+            $("[data-test-id='success-notification'][data-test-id='success-notification'] .notification__content")
+                    .shouldHave(Condition.text(DataGenerator.generateDate(daysToAddForSecondMeeting)));
+    }
+    @Test
+    @DisplayName("Invalid City Name")
+        void shouldInvalidCityName(){
+        $(byText("Запланировать")).click();
+        $("[data-test-id='city'].input_invalid").shouldHave(Condition.visible);
+        }
+    @Test
+    @DisplayName("Meet in city is impossible")
+    void shouldHaveTextWithImpossible(){
+        val validUser = DataGenerator.Registration.generateUser("ru");
+        val daysToAddForFirstMeeting = 4;
+        $("[data-test-id='city'] .input__box .input__control[placeholder='Город']").setValue("Колыма");
         $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(delete());
         $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(DataGenerator
-                .generateDate(daysToAddForSecondMeeting));
+                .generateDate(daysToAddForFirstMeeting));
+        $("[data-test-id='name'] .input__box .input__control[name='name']").setValue(DataGenerator.generateFullName("ru"));
+        $("[data-test-id='phone'] .input__box .input__control[name='phone']").setValue(validUser.getPhone());
+        $("[data-test-id='agreement'] .checkbox__box").click();
         $(byText("Запланировать")).click();
-        $(byText("Перепланировать")).click();
-        $("[data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(10));
-        $("[data-test-id='success-notification'][data-test-id='success-notification'] .notification__content")
-                .shouldHave(Condition.text(DataGenerator.generateDate(daysToAddForSecondMeeting)));
+        $(byText("Доставка в выбранный город недоступна")).shouldHave(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("Meet is impossible in yours select day")
+    void shouldDateHaveError(){
+        val validUser = DataGenerator.Registration.generateUser("ru");
+        val daysToAddForFirstMeeting = 1;
+        $("[data-test-id='city'] .input__box .input__control[placeholder='Город']").setValue(validUser.getCity());
+        $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(delete());
+        $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(DataGenerator
+                .generateDate(daysToAddForFirstMeeting));
+        $("[data-test-id='name'] .input__box .input__control[name='name']").setValue(DataGenerator.generateFullName("ru"));
+        $("[data-test-id='phone'] .input__box .input__control[name='phone']").setValue(validUser.getPhone());
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='date'] .input_invalid .input__box .input__control[placeholder='Дата встречи']").shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("Name isn't a correct")
+    void shouldNameHaveError() {
+        val validUser = DataGenerator.Registration.generateUser("ru");
+        val daysToAddForFirstMeeting = 4;
+        $("[data-test-id='city'] .input__box .input__control[placeholder='Город']").setValue(validUser.getCity());
+        $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(delete());
+        $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(DataGenerator
+                .generateDate(daysToAddForFirstMeeting));
+        $("[data-test-id='name'] .input__box .input__control[name='name']").setValue(DataGenerator.generateFullName("cn"));
+        $("[data-test-id='phone'] .input__box .input__control[name='phone']").setValue(validUser.getPhone());
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(byText("Запланировать")).click();
+        $("[data-test-id='name'].input_invalid .input__box .input__control[name='name']").shouldBe(Condition.visible);
     }
 }
 
